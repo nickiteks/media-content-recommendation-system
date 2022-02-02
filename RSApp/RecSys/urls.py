b3104431ecfb361a87cfd4125984cfbcad485012
nickiteks <55086354+nickiteks@users.noreply.github.com>
2022-02-02 21:09:19 +0400

    some

--- RSApp/RecSys/urls.py
+++ RSApp/RecSys/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('', views.index, name='index'),
+    path('/recommend_film', views.recommend_film, name='recommend')
 ]
--- RSApp/RecSys/views.py
+++ RSApp/RecSys/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'RecSys/index.html', context)
+
+
+def recommend_film(request):
+    context = {}
+    print(request.POST['content_name'])
+    return render(request, 'RecSys/index.html', context)
