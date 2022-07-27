84330cfcab3dd454a9b5b3cbe4b69d6e2c3931db
nickiteks <55086354+nickiteks@users.noreply.github.com>
2022-07-27 20:30:39 +0400

    add postgreSQL

--- RSApp/RSApp/settings.py
+++ RSApp/RSApp/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'RSApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'mediacontent',
+       'USER': 'postgres',
+       'PASSWORD': '12838314',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
